5351e2852977293af4e4038ed4cbd9830ff66ccc
=Memo <=chepkemoicynthia4@gmail.com>
2024-02-06 18:16:40 +0300

    Completed assignment

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,13 @@
 import React from "react";
 
 function Home() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <div>
+      <h1>Home Page</h1>
+      {/* Additional content can be added here if needed */}
+    </div>
+  );
 }
 
 export default Home;
+
